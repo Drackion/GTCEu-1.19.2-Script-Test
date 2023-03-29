8b7c4e4df83d9d6f3f3e1f7c0576cb387c18c03f
Drackion <64641342+Drackion@users.noreply.github.com>
2023-03-30 07:51:31 +1100

    Add files via upload

---
+++ server_scripts/ServerScript.js
@@ -0,0 +1,823 @@
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
+
+ServerEvents.recipes(event => {
+	let inter = 'gtceu:circuit_board.basic'
+	event.recipes.createSequencedAssembly(
+		[
+			Item.of('gtceu:circuit.electronic').withChance(100)
+		], 'gtceu:circuit_board.basic', [
+			event.recipes.createDeploying(inter, [inter, 'gtceu:plate.steel']),
+			event.recipes.createPressing(inter, inter),
+			event.recipes.createFilling(inter, [inter, Fluid.of('gtceu:tin', 144)]),
+			event.recipes.createDeploying(inter, [inter, 'gtceu:cable_single.red_alloy']),
+			event.recipes.createDeploying(inter, [inter, 'gtceu:circuit.vacuum_tube']),
+			event.recipes.createDeploying(inter, [inter, 'gtceu:component.resistor'])
+		]).transitionalItem(inter).loops(2)
+
+	function gtceuCoils(ouput, input, gtfluid, foil, tran){
+		event.recipes.createSequencedAssembly(
+			[
+				Item.of(`gtceu:wire_coil_${ouput}`).withChance(100)
+			], input,
+			[
+				event.recipes.createFilling(tran, [tran, Fluid.of(gtfluid, 8)]),
+				event.recipes.createDeploying(tran, [tran, foil]),
+				event.recipes.createPressing(tran, tran)
+			]
+		).transitionalItem(tran).loops(8)
+	}
+
+	gtceuCoils('cupronickel', 'gtceu:wire_hex.cupronickel', 'gtceu:tin_alloy', 'gtceu:foil.bronze', 'gtceu:wire_hex.cupronickel')
+	gtceuCoils('kanthal', 'gtceu:wire_hex.kanthal', 'gtceu:copper', 'gtceu:foil.aluminium', 'gtceu:wire_hex.kanthal')
+
+	event.recipes.createsifterSifting(
+		[
+			Item.of('gtceu:wire_single.copper').withChance(0.5).toJson(),
+			Item.of('gtceu:wire_single.cupronickel').withChance(0.5).toJson(),
+			Item.of('gtceu:foil.bronze').withChance(0.5).toJson()
+		],
+		[
+			Item.of('gtceu:wire_coil_cupronickel'), 'createsifter:brass_mesh'
+		]
+	).processingTime(20)
+
+
+	//alloy smelter
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 120,
+			inputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "gtceu:wire_single.copper"
+							} 
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "gtceu:wire_single.nickel" 
+							}
+						}
+					}
+				]
+			},
+			outputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 2,
+							ingredient: {
+								item: "gtceu:wire_single.cupronickel"
+							}
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:alloy_smelter",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 16
+					}
+				]
+			}
+		}
+	)
+
+	//assembler
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 100,
+			inputs: {
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 100,
+							fluid: "gtceu:glass"
+						}
+					}
+				],
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 4,
+							ingredient: {
+								item: "kubejs:wire.fine_fluix"
+							}
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 4,
+							ingredient: {
+								item: "gtceu:wire_fine.borosilicate_glass"
+							}
+						}
+					}
+				]
+			},
+			outputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 2,
+							ingredient: {
+								item: "ae2:fluix_glass_cable"
+							}
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:assembler",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 8
+					}
+				]
+			}
+		}
+	)
+
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 100,
+			inputs: {
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 144,
+							fluid: "gtceu:soldering_alloy"
+						}
+					}
+				],
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 4,
+							ingredient: {
+								item: "gtceu:plate_double.vanadium_steel"
+							}
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:fluix_block"
+							}
+						}
+					}
+				]
+			},
+			outputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 2,
+							ingredient: {
+								item: "ae2:not_so_mysterious_cube"
+							}
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:assembler",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 48
+					}
+				]
+			}
+		}
+	)
+
+	//circuit assembler
+	function AE2Proccesors(fluid, printed_1, printed_2, circuit, processor){
+		event.custom(
+			{
+				type: "gtceu:gt_recipe_serializer",
+				duration: 100,
+				inputs: {
+					fluid: [
+						{
+							chance: 1.0,
+							content: {
+								amount: 144,
+								fluid: fluid
+							}
+						}
+					],
+					item: [
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: 1,
+								ingredient: {
+									item: printed_1
+								}
+							}
+						},
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: 1,
+								ingredient: {
+									item: printed_2
+								}
+							}
+						},
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: 1,
+								tag: circuit
+							}
+						}
+					]
+				},
+				outputs: {
+					item: [
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: 4,
+								ingredient: {
+									item: processor
+								}
+							}
+						}
+					]
+				},
+				recipe_type: "gtceu:circuit_assembler",
+				tickInputs: {
+					eu: [
+						{
+							chance: 1.0,
+							content: 40
+						}
+					]
+				}
+			}
+		)
+	}
+	AE2Proccesors("gtceu:red_alloy", "ae2:printed_logic_processor", "ae2:printed_silicon", "forge:circuit/lv", "ae2:logic_processor")
+	AE2Proccesors("gtceu:red_alloy", "ae2:printed_calculation_processor", "ae2:printed_silicon", "forge:circuit/lv", "ae2:calculation_processor")
+	AE2Proccesors("gtceu:red_alloy", "ae2:printed_engineering_processor", "ae2:printed_silicon", "forge:circuit/lv", "ae2:engineering_processor")
+
+	//wiremill
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 120,
+			inputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:fluix_crystal"
+							} 
+						}
+					}
+				]
+			},
+			outputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 8,
+							ingredient: {
+								item: "kubejs:wire.fine_fluix"
+							}
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:wiremill",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 16
+					}
+				]
+			}
+		}
+	)
+
+	//EBF
+	function EBF(Temp, Dur, InItCo1, InIt1, InItCo2, InIt2, InItCo3, InIt3, InFlAm, InFl, OuItCo, OuIt, EU) {
+		event.custom(
+			{
+				type: "gtceu:gt_recipe_serializer",
+				data: {
+					ebf_temp: Temp,
+				},
+				duration: Dur,
+				inputs: {
+					item: [
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: InItCo1,
+								ingredient: {
+									item: InIt1
+								}
+							}
+						},
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: InItCo2,
+								ingredient: {
+									item: InIt2
+								}
+							}
+						},
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: InItCo3,
+								ingredient: {
+									item: InIt3
+								}
+							}
+						}
+					],
+					fluid: [
+						{
+							chance: 1.0,
+							content: {
+								amount: InFlAm,
+								fluid: InFl
+							}
+						}
+					]
+				},
+				outputs: {
+					item: [
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: OuItCo,
+								ingredient: {
+									item: OuIt
+								}
+							}
+						}
+					]
+				},
+				recipe_type: "gtceu:electric_blast_furnace",
+				tickInputs: {
+					eu: [
+						{
+							chance: 1.0,
+							content: EU
+						}
+					]
+				}
+			}
+		)
+	}
+
+	EBF(420, 420, 1, "minecraft:quartz", 1, "minecraft:redstone", 1, "ae2:charged_certus_quartz_crystal", 1000, "minecraft:water", 2, "ae2:fluix_crystal", 69)
+
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			data: {
+				ebf_temp: 1200,
+			},
+			duration: 640,
+			inputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 16,
+							ingredient: {
+								item: "ae2:charged_certus_quartz_crystal"
+							}
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:not_so_mysterious_cube"
+							}
+						}
+					}
+				],
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 1000,
+							fluid: "kubejs:Energized_Ooze"
+						}
+					}
+				]
+			},
+			outputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:mysterious_cube"
+							}
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:electric_blast_furnace",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 120
+					}
+				]
+			}
+		}
+	)
+
+	//Chemical Reactor
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 640,
+			inputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 16,
+							ingredient: {
+								item: "gtceu:dust.phosphorus"
+							}
+						}
+					}
+				],
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 1000,
+							fluid: "kubejs:Mysterious_Ooze"
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							amount: 2304,
+							fluid: "gtceu:glowstone"
+						}
+					}
+				]
+			},
+			outputs: {
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 1000,
+							fluid: "kubejs:Energized_Ooze"
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:chemical_reactor",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 24
+					}
+				]
+			}
+		}
+	)
+
+	//Fluid heater
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 4,
+			inputs: {
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 6,
+							fluid: "gtceu:distilled_water"
+						}
+					}
+				],
+				item: [
+					{
+						chance: 0.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								type: "forge:nbt",
+								count: 1,
+								item: "gtceu:circuit.integrated",
+								nbt: "{Configuration:2}"
+							}
+						}
+					}
+				]
+			},
+			outputs: {
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 960,
+							fluid: "gtceu:steam"
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:fluid_heater",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 16
+					}
+				]
+			}
+		}
+	)
+
+	//Extractor
+
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 160,
+			inputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:not_so_mysterious_cube"
+							}
+						}
+					}
+				]
+			},
+			outputs: {
+				fluid: [
+					{
+						chance: 1.0,
+						content: {
+							amount: 1000,
+							fluid: "kubejs:Mysterious_Ooze"
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:extractor",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 40
+					}
+				]
+			}
+		}
+	)
+
+	//Centrifuge
+
+	event.custom(
+		{
+			type: "gtceu:gt_recipe_serializer",
+			duration: 80,
+			inputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:mysterious_cube"
+							}
+						}
+					}
+				]
+			},
+			outputs: {
+				item: [
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:silicon_press"
+							}
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:calculation_processor_press"
+							}
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:engineering_processor_press"
+							}
+						}
+					},
+					{
+						chance: 1.0,
+						content: {
+							type: "gtceu:sized",
+							count: 1,
+							ingredient: {
+								item: "ae2:logic_processor_press"
+							}
+						}
+					}
+				]
+			},
+			recipe_type: "gtceu:centrifuge",
+			tickInputs: {
+				eu: [
+					{
+						chance: 1.0,
+						content: 48
+					}
+				]
+			}
+		}
+	)
+
+	//Forming Press
+	function AE2Press(input, ae2press, output) {
+		event.custom(
+			{
+				type: "gtceu:gt_recipe_serializer",
+				duration: 120,
+				inputs: {
+					item: [
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: 1,
+								ingredient: {
+									item: input
+								} 
+							}
+						},
+						{
+							chance: 0.0,
+							content: {
+								type: "gtceu:sized",
+								count: 1,
+								ingredient: {
+									item: ae2press
+								}
+							}
+						}
+					]
+				},
+				outputs: {
+					item: [
+						{
+							chance: 1.0,
+							content: {
+								type: "gtceu:sized",
+								count: 1,
+								ingredient: {
+									item: output
+								}
+							}
+						}
+					]
+				},
+				recipe_type: "gtceu:forming_press",
+				tickInputs: {
+					eu: [
+						{
+							chance: 1.0,
+							content: 16
+						}
+					]
+				}
+			}
+		)
+	}
+	
+	AE2Press("minecraft:gold_ingot", "ae2:logic_processor_press", "ae2:printed_logic_processor")
+	AE2Press("minecraft:diamond", "ae2:engineering_processor_press", "ae2:printed_engineering_processor")
+	AE2Press("ae2:silicon", "ae2:silicon_press", "ae2:printed_silicon")
+	AE2Press("ae2:certus_quartz_crystal", "ae2:calculation_processor_press", "ae2:printed_calculation_processor")
+
+
+})
+
+ServerEvents.tags('item', event => {
+	event.get('createsifter:meshes').add('kubejs:mesh_bronze')
+})
+
+GTCEuServerEvents.recipe(event => {
+	event.builder("IndustrialHive_recipe_type", "IndustrialHive_type")
+		.itemInputs("minecraft:stone")
+		.inputFluids("minecraft:water 1000")
+		.itemOutputs("3x minecraft:dirt")
+		.duration(80)
+		.EUt(24)
+	.save(event.provider);
+
+	
+	event.builder("greenhouse_recipe_type", "greenhouse_type")
+		.chance(0)
+		.itemInputs("minecraft:oak_sapling")
+		.circuitMeta(1)
+		.chance(1)
+		.inputFluids("minecraft:water 1000")
+		.itemOutputs("2x minecraft:oak_sapling", "64x minecraft:oak_log")
+		.duration(640)
+		.EUt(30)
+	.save(event.provider);
+
+	
+	
+	//.notConsumable("minecraft:oak_sapling")	
+	//GreenHouse("minecraft:oak_sapling", 1, "minecraft:air", "2x minecraft:oak_sapling", "64x minecraft:oak_log", "minecraft:air")
+})
---
+++ startup_scripts/StartupScript.js
@@ -0,0 +1,97 @@
+// priority: 0
+
+console.info('Hello, World! (You will only see this line once in console, during startup)')
+
+StartupEvents.registry('item', event => {
+	// Register new items here
+	// event.create('example_item').displayName('Example Item')
+	event.create('wire.fine_fluix').displayName('Fine Fluix Wire')
+	event.create('conveyor.primitive').displayName('Primitive Conveyor')
+	event.create('motor.primitive').displayName('Primitive Motor')
+	event.create('robot.arm.primitive').displayName('Primitive Robot Arm')
+	event.create('mesh_bronze','createsifter:mesh').displayName('Bronze Mesh').parentModel("createsifter:block/meshes/custom_mesh").texture("1", "kubejs:item/mesh_bronze")
+	event.create('catalyst.copper_bee').displayName('Copper Bee Catalyst')
+	event.create('comb.copper_group').displayName('Copper Group Comb').texture('kubejs:item/comb.base').color(0, 0xFF6900)
+})
+
+StartupEvents.registry('fluid', event => {
+	event.create('mysterious_ooze')
+		.thickTexture(0x500bbf)
+		.displayName('Mysterious Ooze')
+		.noBlock()
+	event.create('energized_ooze')
+		.thickTexture(0x6819e6)
+		.displayName('Energized Ooze')
+		.noBlock()
+	event.create('liquid_pollen')
+		.thickTexture(0xf5e213)
+		.displayName('Liquid Pollen')
+		.noBlock()
+})
+
+
+console.info('RECIPE TYPE MAN')
+GTCEuStartupEvents.recipeType(event => {
+	//.setIOSize('minItemInput', 'maxItemInput', 'minItemOutput', 'maxItemOutput', 'minFluidInput', 'maxFluidInput', 'minFluidOutput','maxFluidOutput')
+	event.create("greenhouse_recipe_type").setIOSize(1, 3, 1, 4, 1, 2, 0, 0)
+		.setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+		.setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+	.setSound(GTSoundEntries.COOLING)
+
+	event.create("IndustrialHive_recipe_type").setIOSize(1, 3, 1, 4, 0, 2, 0, 2)
+		.setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+		.setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+	.setSound(GTSoundEntries.COOLING)
+})
+
+GTCEuStartupEvents.machine(event => {
+	console.info('Green HOUSE MAN')
+	event.simpleMultiblock("greenhouse_multiblock")
+		.rotationState(RotationState.NON_Y_AXIS)
+		.recipeType("greenhouse_recipe_type")
+		.appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
+		.pattern(definition => FactoryBlockPattern.start()
+			.aisle("CCC", "CGC", "CGC", "CLC", "CCC")
+			.aisle("CMC", "GSG", "G#G", "LIL", "COC")
+			.aisle("CKC", "CGC", "CGC", "CLC", "CCC")
+			.where('K', Predicates.controller(Predicates.blocks(definition.get())))
+			.where('M', Predicates.blocks("minecraft:moss_block"))
+			.where('G', Predicates.blocks("ae2:quartz_glass"))
+			.where('S', Predicates.blocks("minecraft:oak_sapling"))
+			.where('I', Predicates.blocks("minecraft:glowstone"))
+			.where('L', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
+			.where('C', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()).setMinGlobalLimited(1)
+				.or(Predicates.autoAbilities(definition.getRecipeType())))
+			.where('O', Predicates.abilities(PartAbility.MUFFLER).setExactLimit(1))
+			.where('#', Predicates.air())
+			.build())
+		.workableCasingRenderer("gtceu:block/casings/solid/machine_casing_solid_steel",
+			"gtceu:block/multiblock/implosion_compressor", false)
+	.register();
+
+	event.simpleMultiblock("Industrial Hive")
+		.rotationState(RotationState.NON_Y_AXIS)
+		.recipeType("IndustrialHive_recipe_type")
+		.appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
+		.pattern(definition => FactoryBlockPattern.start()
+			.aisle("CCCCC", "CGGGC", "CGGGC", "CLLLC", "CCCCC")
+			.aisle("CMMMC", "GF#FG", "G###G", "LIIIL", "CCCCC")
+			.aisle("CMMMC", "G#H#G", "G###G", "LIIIL", "CCOCC")
+			.aisle("CMMMC", "GF#FG", "G###G", "LIIIL", "CCCCC")
+			.aisle("CCKCC", "CGGGC", "CGGGC", "CLLLC", "CCCCC")
+			.where('K', Predicates.controller(Predicates.blocks(definition.get())))
+			.where('M', Predicates.blocks("minecraft:moss_block"))
+			.where('G', Predicates.blocks("ae2:quartz_glass"))
+			.where('H', Predicates.blocks("minecraft:beehive"))
+			.where('F', Predicates.blocks("minecraft:poppy"))
+			.where('I', Predicates.blocks("minecraft:glowstone"))
+			.where('L', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
+			.where('C', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()).setMinGlobalLimited(1)
+				.or(Predicates.autoAbilities(definition.getRecipeType())))
+			.where('O', Predicates.abilities(PartAbility.MUFFLER).setExactLimit(1))
+			.where('#', Predicates.air())
+			.build())
+		.workableCasingRenderer("gtceu:block/casings/solid/machine_casing_solid_steel",
+			"gtceu:block/multiblock/implosion_compressor", false)
+	.register();
+})
